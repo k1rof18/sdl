b67adf1c25b0aa1ad763be39c051e6d31a80ce7a
k-ichirof <abachoppa1892@gmail.com>
2025-03-16 15:57:27 +0900

    不要なの消した

--- app/Main.hs
+++ app/Main.hs
@@ -3,18 +3,14 @@ module Main (main) where
 import Domain.Client (ClientId (ClientId))
 import Domain.Project (ProjectId (..))
 import Domain.Recruit (RecruitId (..))
-import Gateway.FirstStageProjectApplicantGateway (FirstStageProjectApplicantGateway (..), runFirstStageProjectApplicantGateway)
 import Gateway.ProjectApplicantGateway (ProjectApplicantGateway (..), runProjectApplicantGateway)
 import Gateway.ProjectGateway (ProjectGateway (..), runProjectGateway)
 import Gateway.ProjectWorkerGateway (ProjectWorkerGateway (..), runProjectWorkerGateway)
 import Gateway.RecruitGateway (RecruitGateway (..), runRecruitGateway)
-import Gateway.SecondStageProjectApplicantGateway (SecondStageProjectApplicantGateway (..), runSecondStageProjectApplicantGateway)
 import UseCase.GetRecruit (exec)
-import UseCase.ListFirstStageProjectApplicants (exec)
 import UseCase.ListProjectApplicants (exec)
 import UseCase.ListProjectWorkers (exec)
 import UseCase.ListProjects (exec)
-import UseCase.ListSecondStageProjectApplicants (exec)
 
 getRecruit :: String -> IO ()
 getRecruit recruitId = do
@@ -42,18 +38,6 @@ listProjectWorker clientId recruitId = do
   mapM_ print result
   print "!!!!!!!!!!!!!!!"
 
-listFirstStageProjectApplicants :: String -> String -> IO ()
-listFirstStageProjectApplicants clientId projectId = do
-  result <- runFirstStageProjectApplicantGateway $ UseCase.ListFirstStageProjectApplicants.exec (ClientId clientId) (ProjectId projectId)
-  mapM_ print result
-  print "========================"
-
-listSecondStageProjectApplicants :: String -> String -> IO ()
-listSecondStageProjectApplicants clientId projectId = do
-  result <- runSecondStageProjectApplicantGateway $ UseCase.ListSecondStageProjectApplicants.exec (ClientId clientId) (ProjectId projectId)
-  mapM_ print result
-  print "========================"
-
 main :: IO ()
 main = do
   print "========================"
@@ -61,7 +45,5 @@ main = do
   getRecruit "d20fa5f5-7b96-1ff1-d8b3-f7565aeecbb7"
   listProject
   listProjectApplicant "cf71705d-de27-40fe-9181-e190e0a65eb6" "f47b1b3e-7f3b-4b0b-8b3d-3b1b1f3b7b4f"
-  listFirstStageProjectApplicants "cf71705d-de27-40fe-9181-e190e0a65eb6" "f47b1b3e-7f3b-4b0b-8b3d-3b1b1f3b7b4f"
-  listSecondStageProjectApplicants "cf71705d-de27-40fe-9181-e190e0a65eb6" "f47b1b3e-7f3b-4b0b-8b3d-3b1b1f3b7b4f"
 
   listProjectWorker "cf71705d-de27-40fe-9181-e190e0a65eb6" "f47b1b3e-7f3b-4b0b-8b3d-3b1b1f3b7b4f"
